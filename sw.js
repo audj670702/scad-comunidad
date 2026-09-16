668e9105f3cd47be725598199656b70022195d0c
SCaD <gestor.isp@gmail.com>
2026-09-16 11:41:22 -0600

    SCaD Comunidad v0.3.7: actualizar caché PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='scad-comunidad-v0.3.6';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./img/logo_scad_comunidad.png','./img/icon_scad_comunidad_192.png','./img/icon_scad_comunidad_512.png','./img/scad_comunidad_favicon.png'];
+const CACHE='scad-comunidad-v0.3.7';
+const ASSETS=['./','./index.html','./styles.css','./install.css','./app.js','./install.js','./manifest.json','./img/logo_scad_comunidad.png','./img/icon_scad_comunidad_192.png','./img/icon_scad_comunidad_512.png','./img/scad_comunidad_favicon.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;if(event.request.url.includes('/_functions/'))return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
