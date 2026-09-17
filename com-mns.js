a877f0f3eb984a8c0478fefcd4b2d8af10944bcc
SCaD <gestor.isp@gmail.com>
2026-09-17 13:37:26 -0600

    Bump COMUNIDAD MNS integration version to v3.13

--- com-mns.js
+++ com-mns.js
@@ -1,8 +1,9 @@
 import { createClient, OAuthStrategy } from 'https://esm.sh/@wix/sdk';
 import { functions } from 'https://esm.sh/@wix/http-functions@1.0.0';
 
+// SCaD COMUNIDAD · Integración MNS · v3.13
 const CHANNEL='MNS_FRONTEND';
-const FRAME_URL='mns-frontend-v052.html?v=0.5.9';
+const FRAME_URL='mns-frontend-v052.html?v=0.5.11';
 const CLIENT_ID='8943652e-6424-4b27-961b-9486abcc97b7';
 const SITE_ID='e9c5ce53-8342-4146-acd9-3468abb10cb0';
 const REDIRECT_URI='https://comunidad.scad.mx/';
