0ee445fcbf2ccf77bd9f13bba65531f161a84ccd
SCaD <gestor.isp@gmail.com>
2026-09-17 20:50:17 -0600

    Free EO identity logo from visual container v0.3.13

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const VERSION='0.3.12';
+const VERSION='0.3.13';
 const AUTH_URL='https://www.scad.mx/com-autenticacion';
 const CONTEXT_URL='https://www.scad.mx/_functions/comPwaContext';
 const MIS_CURSOS_URL='https://www.scad.mx/mis-cursos';
